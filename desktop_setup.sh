94683f450fd1e5714f5b6ebfad5b798d091f6ab3
Chris Snow <Chris.Snow@monitise.com>
2013-12-27 13:18:01 +0000

    use latest eclipse and add desktop icons

--- desktop_setup.sh
+++ desktop_setup.sh
@@ -7,3 +7,59 @@ yum groupinstall -y "X Window System" Desktop
 yum install -y firefox
 cp /etc/inittab /etc/inittab.bak 
 sed -i 's/id:3:initdefault:/id:5:initdefault:/' /etc/inittab
+
+###############
+# setup eclipse
+###############
+
+wget http://www.mirrorservice.org/sites/download.eclipse.org/eclipseMirror/technology/epp/downloads/release/luna/M4/eclipse-jee-luna-M4-linux-gtk-x86_64.tar.gz
+su -c "tar -zxvf eclipse-jee-luna-M4-linux-gtk-x86_64.tar.gz -C /home/vagrant/"
+chown -R vagrant:vagrant /home/vagrant/eclipse
+rm /root/eclipse-jee-luna-M4-linux-gtk-x86_64.tar.gz
+
+##############################
+# Openstack dashboard launcher
+##############################
+
+cat << EOF > /home/vagrant/Desktop/openstack.desktop
+#!/usr/bin/env xdg-open
+
+[Desktop Entry]
+Version=1.0
+Encoding=UTF-8
+Exec=firefox %u http://localhost/dashboard
+Icon=firefox
+Terminal=false
+Type=Application
+StartupWMClass=Firefox-bin
+MimeType=text/html;text/xml;application/xhtml+xml;application/vnd.mozilla.xul+xml;text/mml;
+StartupNotify=true
+X-Desktop-File-Install-Version=0.15
+Categories=Network;WebBrowser;
+GenericName[en_US.UTF-8]=Openstack Dashboard
+Comment[en_US.UTF-8]=Openstack Dashboard
+Name[en_US]=Openstack Dashboard
+EOF
+
+chown vagrant:vagrant /home/vagrant/Desktop/openstack.desktop
+chmod 770 /home/vagrant/Desktop/openstack.desktop
+
+##############################
+# eclipse launcher
+##############################
+
+cat << EOF > /home/vagrant/Desktop/eclipse.desktop
+#!/usr/bin/env xdg-open
+
+[Desktop Entry]
+Type=Application
+Name=Eclipse
+Comment=Eclipse Integrated Development Environment
+Icon=eclipse
+Exec=/home/vagrant/eclipse/eclipse -data /home/vagrant/workspace
+Terminal=false
+Categories=Development;IDE;Java;
+EOF
+
+chown vagrant:vagrant /home/vagrant/Desktop/eclipse.desktop
+chmod 770 /home/vagrant/Desktop/eclipse.desktop
--- stratos_developer_setup.sh
+++ stratos_developer_setup.sh
@@ -3,19 +3,28 @@
 set -e
 set -x
 
-yum install -y java-1.7.0-openjdk-devel git eclipse-jdt
+yum install -y java-1.7.0-openjdk-devel git 
 
-sudo -i -u vagrant \
-   git clone https://git-wip-us.apache.org/repos/asf/incubator-stratos.git 
+#############
+# setup maven
+#############
 
 wget http://mirrors.gigenet.com/apache/maven/maven-3/3.0.5/binaries/apache-maven-3.0.5-bin.tar.gz
 su -c "tar -zxvf apache-maven-3.0.5-bin.tar.gz -C /opt/" 
+rm /root/apache-maven-3.0.5-bin.tar.gz
 
 cat << EOF > /etc/profile.d/maven.sh
 export M2_HOME=/opt/apache-maven-3.0.5
 export M2=\$M2_HOME/bin
 export PATH=$M2:$PATH
 EOF
+
+############################
+# checkout and build stratos
+############################
+
+sudo -i -u vagrant \
+   git clone https://git-wip-us.apache.org/repos/asf/incubator-stratos.git 
 
 M2_HOME=/opt/apache-maven-3.0.5
 STRATOS_SRC=/home/vagrant/incubator-stratos
